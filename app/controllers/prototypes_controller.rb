c57f338cd5e3de877bb47b062672af15288cfbf2
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-05-17 18:18:52 +0900

    直接URLを入力しても投稿者以外編集できないように修正。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit,:destroy]
 
   def index
     @prototypes = Prototype.all
@@ -26,8 +26,8 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to action: :index
+    unless @prototype.user == current_user
+      redirect_to root_path
     end
   end
 
