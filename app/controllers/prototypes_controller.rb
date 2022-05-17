51270afb488690cd7ab3f780ac468137a440a757
MaaaaaaaSa <yakyubou.1222@icloud.com>
2022-05-17 16:28:11 +0900

    削除できるとトップページへ戻れるように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -42,7 +42,9 @@ class PrototypesController < ApplicationController
 
   def destroy
     prototype = Prototype.find(params[:id])
-    prototype.destroy
+    if prototype.destroy
+      redirect_to root_path
+    end
   end
   
   private
